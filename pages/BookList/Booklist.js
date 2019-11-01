41067f6a8462a9d38b9ed97383c4d5ab4e2b16d6
huang13849 <huang13849@hotmail.com>
2019-11-01 17:07:55 +0800

    Day3

--- pages/BookList/Booklist.js
+++ pages/BookList/Booklist.js
@@ -5,15 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name:'Coderwhy',
-    students:[
-      { id:100,name:'asad',age:'30'},
-      { id: 101, name: 'ssda', age: '21' },
-      { id: 102, name: 'vvv', age: '28' },
-      { id: 103, name: 'ddd', age: '18' }
-    ],
-    counter:0,
-    message:'12321'
+    booklist:[]
   },
   handleButtonClick(){
     console.log('按钮发生'),
@@ -29,55 +21,66 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad')
+    wx.request({
+      url: 'https://hfnmyrxsof57oiu-atbhuangfra.adb.us-ashburn-1.oraclecloudapps.com/ords/huangfra/book1/',
+      success:(res)=>{
+          console.log(res);
+          const data =res.data.items;
+          this.data.booklist=data;
+          this.setData({
+            booklist:data
+          })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('onReachBottom')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log('onShareAppMessage')
   }
 })
