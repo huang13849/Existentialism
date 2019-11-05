f68a89a8580f0716175fb0f58ed8480949b3e660
huang13849 <huang13849@hotmail.com>
2019-11-05 15:33:26 +0800

    Day4

--- pages/BookList/Booklist.js
+++ pages/BookList/Booklist.js
@@ -5,32 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-    booklist:[]
+    booklist: []
   },
-  handleButtonClick(){
-    console.log('按钮发生'),
-    console.log(this.data.counter)
-    this.setData({
-      counter:this.data.counter +=1
+  toRefresh() {
+    console.log('toRefresh');
+    wx.request({
+      url: 'https://hfnmyrxsof57oiu-atbhuangfra.adb.us-ashburn-1.oraclecloudapps.com/ords/huangfra/book1/',
+      success: (res) => {
+        console.log(res);
+        const data = res.data.items;
+        this.data.booklist = data;
+        this.setData({
+          booklist: data
+        })
+      }
     })
-  },
-  handleBtnClick(){
-    this.setData({message:'aasda'})
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    console.log('onLoad')
-    wx.request({
-      url: 'https://hfnmyrxsof57oiu-atbhuangfra.adb.us-ashburn-1.oraclecloudapps.com/ords/huangfra/book1/',
-      success:(res)=>{
-          console.log(res);
-          const data =res.data.items;
-          this.data.booklist=data;
-          this.setData({
-            booklist:data
-          })
+  onLoad: function(options) {
+    console.log('onLoad');
+    wx.cloud.init(
+      {env:'exist-0nmi1'}
+    );
+    const db =wx.cloud.database()
+    db.collection('books_v2').get({
+      success:res=>{
+        console.log(res);
+        const data = res.data
+        this.setData({
+          booklist:data
+        })
       }
     })
   },
@@ -38,49 +44,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     console.log('onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
     console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
     console.log('onUnload')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
     console.log('onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
     console.log('onReachBottom')
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     console.log('onShareAppMessage')
   }
 })
