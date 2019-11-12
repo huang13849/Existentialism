2ffc17b0d6c32b264712583f0f0fb7c2ed9be7ea
huang13849 <huang13849@hotmail.com>
2019-11-13 00:59:19 +0800

    Day6 分类选择完成

--- pages/BookList/Booklist.js
+++ pages/BookList/Booklist.js
@@ -1,38 +1,40 @@
 // pages/BookList/Booklist.js
+const request = require("../../utils/requests");
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    booklist:[]
+    leads: [],
+    booklist: [],
+    book_categories: ['文学', '历史', '心理', '传记', '经管'],
+    book_category_index: 0
   },
   toRefresh() {
-    console.log('toRefresh');
-    wx.request({
-      url: 'https://hfnmyrxsof57oiu-atbhuangfra.adb.us-ashburn-1.oraclecloudapps.com/ords/huangfra/book1/',
-      success: (res) => {
-        console.log(res);
-        const data = res.data.items;
-        this.data.booklist = data;
-        this.setData({
-          booklist: data
-        })
-      }
-    })
+    this.data.book_category_index++,
+      this.getBookCategory()
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log('onLoad');
-    wx.cloud.init(
-      {env:'exist-0nmi1'}
-    );
-    const db =wx.cloud.database()
-    db.collection('books').get({
+    this.getBookCategory()
+  },
+  getBookCategory: function() {
+    if (this.data.book_categories.length - this.data.book_category_index == 0) {
+      this.data.book_category_index = 0;
+    }
+    console.log(this.data.book_category_index + '@@@' + this.data.book_categories[this.data.book_category_index] + '###' + this.data.book_categories.length)
+    wx.cloud.init({
+      env: 'exist-0nmi1'
+    });
+    const db = wx.cloud.database();
+    db.collection('articles').where({
+      article_category: this.data.book_categories[this.data.book_category_index]
+    }).get({
       success: res => {
-        console.log(res);
         const data = res.data
         this.setData({
           booklist: data
--- utils/requests.js
+++ utils/requests.js
@@ -1,28 +1,56 @@
 var api = require('./config.js');
 //网络请求模块
+
+
 function request(url, data, successCb, errorCb, completeCb) {
-    wx.request({
-        url: url,
-        method: 'GET',
-        data: data,
-        success: successCb,
-        error: errorCb,
-        complete: completeCb
-    });
+  wx.request({
+    url: url,
+    method: 'GET',
+    data: data,
+    success: successCb,
+    error: errorCb,
+    complete: completeCb
+  });
+}
+
+function requestBookList() {
+  console.log(book_category_index + '@@@' + book_categories[book_category_index])
+  wx.cloud.init({
+    env: 'exist-0nmi1'
+  });
+  const db = wx.cloud.database();
+  db.collection('articles').where({
+
+  }).get({
+    success: res => {
+      const data = res.data
+      this.setData({
+        booklist: data
+      })
+    }
+  })
 }
 
 //搜索图书 
 function searchBook(data, successCb, errorCb, completeCb) {
-    request(api.searchBook, data, successCb, errorCb, completeCb);
+  request(api.searchBook, data, successCb, errorCb, completeCb);
 }
 //获取图书详细信息
 function getBookById(id, successCb, errorCb, completeCb) {
-    request(api.getBookById+id, "", successCb, errorCb, completeCb);
+  request(api.getBookById + id, "", successCb, errorCb, completeCb);
 }
 //获取丛书列表
 function getBookList(id, data, successCb, errorCb, completeCb) {
-    request(api.getBookList.replace(':id', id), data, successCb, errorCb, completeCb);
+  request(api.getBookList.replace(':id', id), data, successCb, errorCb, completeCb);
 }
 
+function getBookCategory() {
+  requestBookList();
+}
 //
-module.exports = { searchBook: searchBook, getBookById: getBookById, getBookList:getBookList}
+module.exports = {
+  searchBook: searchBook,
+  getBookById: getBookById,
+  getBookList: getBookList,
+  getBookCategory: getBookCategory
+}
